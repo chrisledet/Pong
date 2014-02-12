a5a4539b9a36e82cf6e66d397c201660c917c54a
Chris Ledet <christopher.ledet@gmail.com>
2014-02-12 00:50:49 -0500

    Smoother movement

--- Pong/LEDMyScene.m
+++ Pong/LEDMyScene.m
@@ -15,24 +15,24 @@
 
 #define LED_PONG_PADDLE_SIZE    CGSizeMake(35, 150)
 #define LED_PONG_PADDING        20
-#define LED_PONG_PADDLE_SPEED   15
+#define LED_PONG_PADDLE_SPEED   20
 
 @interface LEDMyScene()
 
 @property (nonatomic, strong) SKSpriteNode *playerPaddle;
 
+@property (nonatomic, assign) BOOL moveUp;
+@property (nonatomic, assign) BOOL moveDown;
+
 @end
 
 @implementation LEDMyScene
 
--(id)initWithSize:(CGSize)size {    
+- (id)initWithSize:(CGSize)size {
+
     if (self = [super initWithSize:size]) {
-        /* Setup your scene here */
 
         self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
-
-        NSLog(@"self.size: %@", NSStringFromSize(self.size));
-        
         self.backgroundColor = [SKColor colorWithRed:0.20 green:0.20 blue:0.20 alpha:1.0];
 
         self.playerPaddle = [SKSpriteNode spriteNodeWithColor:[NSColor whiteColor] size:LED_PONG_PADDLE_SIZE];
@@ -43,22 +43,39 @@
     return self;
 }
 
--(void)keyDown:(NSEvent *)keyEvent
-{
-    if ([keyEvent keyCode] == LED_PONG_MOVE_UP || [keyEvent keyCode] == LED_PONG_MOVE_UP_ALT)
-    {
-        CGPoint currentPosition = self.playerPaddle.position;
-        self.playerPaddle.position = CGPointMake(currentPosition.x, currentPosition.y + LED_PONG_PADDLE_SPEED);
+#pragma mark - OS X Event Handling
+
+- (void)keyUp:(NSEvent *)keyEvent {
+    [self handleKeyEvent:keyEvent keyDown:NO];
+}
+
+-(void)keyDown:(NSEvent *)keyEvent {
+    [self handleKeyEvent:keyEvent keyDown:YES];
+}
+
+- (void)handleKeyEvent:(NSEvent*)keyEvent keyDown:(BOOL)isKeyDown {
+
+    if ([keyEvent keyCode] == LED_PONG_MOVE_UP || [keyEvent keyCode] == LED_PONG_MOVE_UP_ALT) {
+        self.moveUp = isKeyDown;
     }
-    else if ([keyEvent keyCode] == LED_PONG_MOVE_DOWN || [keyEvent keyCode] == LED_PONG_MOVE_DOWN_ALT)
-    {
-        CGPoint currentPosition = self.playerPaddle.position;
-        self.playerPaddle.position = CGPointMake(currentPosition.x, currentPosition.y - LED_PONG_PADDLE_SPEED);
+    else if ([keyEvent keyCode] == LED_PONG_MOVE_DOWN || [keyEvent keyCode] == LED_PONG_MOVE_DOWN_ALT) {
+        self.moveDown = isKeyDown;
     }
 }
 
--(void)update:(CFTimeInterval)currentTime {
+#pragma mark - Update Frame
+
+- (void)update:(CFTimeInterval)currentTime {
     /* Called before each frame is rendered */
+
+    if (self.moveUp) {
+        CGPoint currentPosition = self.playerPaddle.position;
+        self.playerPaddle.position = CGPointMake(currentPosition.x, currentPosition.y + LED_PONG_PADDLE_SPEED);
+    } else if (self.moveDown) {
+        CGPoint currentPosition = self.playerPaddle.position;
+        self.playerPaddle.position = CGPointMake(currentPosition.x, currentPosition.y - LED_PONG_PADDLE_SPEED);
+    }
+
 }
 
 @end
